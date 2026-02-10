69152d043ff6e3b9f290b48ae48da4d634cfa15d
Sahil Ghadi <sahilghadi514@gmail.com>
2026-02-10 15:33:56 +0530

    fixes

--- script.py
+++ script.py
@@ -90,9 +90,9 @@ def get_match_data(url):
                     '--disable-blink-features=AutomationControlled' # Hides webdriver flag
                 ]
             )
-            context = browser.new_context(
-                viewport={'width': 1920, 'height': 1080},
-                user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+            context = p.chromium.launch_persistent_context(
+                user_data_dir="./userdata",
+                headless=True
             )
             page = context.new_page()
             
